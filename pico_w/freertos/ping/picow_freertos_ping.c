4d001eba7c1ed86433710c150033aee86a6643f0
katonobu <katonobu4649@gmail.com>
2023-01-19 16:07:47 +0000

    change ping function to MQTT function.

--- pico_w/freertos/ping/picow_freertos_ping.c
+++ pico_w/freertos/ping/picow_freertos_ping.c
@@ -11,18 +11,126 @@
 
 #include "FreeRTOS.h"
 #include "task.h"
-#include "ping.h"
+#include "lwip/apps/mqtt.h"
 
-#ifndef PING_ADDR
-#define PING_ADDR "142.251.35.196"
-#endif
 #ifndef RUN_FREERTOS_ON_CORE
 #define RUN_FREERTOS_ON_CORE 0
 #endif
 
 #define TEST_TASK_PRIORITY				( tskIDLE_PRIORITY + 1UL )
 
+
+//static ip_addr_t mqtt_ip = IPADDR4_INIT_BYTES(34,77,13,55);
+static ip_addr_t mqtt_ip = IPADDR4_INIT_BYTES(172,16,82,232);
+static mqtt_client_t* mqtt_client;
+
+enum mqtt_stt_t {
+    STT_AFTER_RESET,
+    STT_NET_CONNECTING,
+    STT_MQTT_CONNECTING,
+    STT_MQTT_CONNECTED,
+    STT_MQTT_DISCONNECTED,
+};
+
+static const struct mqtt_connect_client_info_t mqtt_client_info =
+{
+    "RZPPW",
+    NULL, /* user */
+    NULL, /* pass */
+    100,  /* keep alive */
+    NULL, /* will_topic */
+    NULL, /* will_msg */
+    0,    /* will_qos */
+    0     /* will_retain */
+#if LWIP_ALTCP && LWIP_ALTCP_TLS
+    , NULL
+#endif
+};
+
+
+struct mqtt_user_info_t {
+    enum mqtt_stt_t mqtt_stt;
+    uint32_t led_interval;
+    bool led_stt;
+};
+
+static struct mqtt_user_info_t user_info;
+
+static void update_mqtt_stt(enum mqtt_stt_t stt) {
+    LWIP_PLATFORM_DIAG(("MQTT stt update from %d to %d\n", user_info.mqtt_stt, stt));
+    user_info.mqtt_stt = stt;
+    if (stt < STT_MQTT_CONNECTED) {
+        user_info.led_interval = 50;
+    } else if (stt == STT_MQTT_CONNECTED) {
+        user_info.led_interval = 200;
+    } else {
+        user_info.led_interval = 1000;
+    }
+}
+
+static void
+mqtt_incoming_data_cb(void *arg, const u8_t *data, u16_t len, u8_t flags)
+{
+  const struct mqtt_connect_client_info_t* client_info = (const struct mqtt_connect_client_info_t*)arg;
+  LWIP_UNUSED_ARG(data);
+
+  LWIP_PLATFORM_DIAG(("MQTT client \"%s\" data cb: len %d, flags %d\n", client_info->client_id,
+          (int)len, (int)flags));
+}
+
+static void
+mqtt_incoming_publish_cb(void *arg, const char *topic, u32_t tot_len)
+{
+  const struct mqtt_connect_client_info_t* client_info = (const struct mqtt_connect_client_info_t*)arg;
+
+  LWIP_PLATFORM_DIAG(("MQTT client \"%s\" publish cb: topic %s, len %d\n", client_info->client_id,
+          topic, (int)tot_len));
+}
+
+static void
+mqtt_request_cb(void *arg, err_t err)
+{
+  const struct mqtt_connect_client_info_t* client_info = (const struct mqtt_connect_client_info_t*)arg;
+
+  LWIP_PLATFORM_DIAG(("MQTT client \"%s\" request cb: err %d\n", client_info->client_id, (int)err));
+}
+
+static void
+mqtt_connection_cb(mqtt_client_t *client, void *arg, mqtt_connection_status_t status)
+{
+  const struct mqtt_connect_client_info_t* client_info = (const struct mqtt_connect_client_info_t*)arg;
+  LWIP_UNUSED_ARG(client);
+
+  LWIP_PLATFORM_DIAG(("MQTT client \"%s\" connection cb: status %d\n", client_info->client_id, (int)status));
+
+  if (status == MQTT_CONNECT_ACCEPTED) {
+    update_mqtt_stt(STT_MQTT_CONNECTED);
+    mqtt_subscribe(client, "hello", 1, mqtt_request_cb, LWIP_CONST_CAST(void*, client_info));
+  } else {
+    update_mqtt_stt(STT_MQTT_DISCONNECTED);
+    mqtt_unsubscribe(client, "hello", mqtt_request_cb, LWIP_CONST_CAST(void*, client_info));
+  }
+}
+
+void
+mqtt_example_init(void)
+{
+    mqtt_client = mqtt_client_new();
+
+    mqtt_set_inpub_callback(mqtt_client,
+        mqtt_incoming_publish_cb,
+        mqtt_incoming_data_cb,
+        LWIP_CONST_CAST(void*, &mqtt_client_info));
+
+    mqtt_client_connect(mqtt_client,
+        &mqtt_ip, MQTT_PORT,
+        mqtt_connection_cb, LWIP_CONST_CAST(void*, &mqtt_client_info),
+        &mqtt_client_info);
+}
+
+
 void main_task(__unused void *params) {
+    update_mqtt_stt(STT_NET_CONNECTING);
     if (cyw43_arch_init()) {
         printf("failed to initialise\n");
         return;
@@ -35,14 +143,17 @@ void main_task(__unused void *params) {
     } else {
         printf("Connected.\n");
     }
+    update_mqtt_stt(STT_MQTT_CONNECTING);
 
-    ip_addr_t ping_addr;
-    ip4_addr_set_u32(&ping_addr, ipaddr_addr(PING_ADDR));
-    ping_init(&ping_addr);
+    mqtt_example_init();
 
+    user_info.led_interval = 100;
     while(true) {
         // not much to do as LED is in another task, and we're using RAW (callback) lwIP API
-        vTaskDelay(100);
+        vTaskDelay(user_info.led_interval);
+        user_info.led_stt = !user_info.led_stt;
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, user_info.led_stt);
+
     }
 
     cyw43_arch_deinit();
