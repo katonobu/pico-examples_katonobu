f810696ff298b1038ced7d67f2382a6252a88170
Nobuo Kato <katonobu4649@gmail.com>
2023-02-15 00:23:37 +0900

    Use pico_rand instead of pico_lwip_rand.

--- tinyusb_net/tinyusb_net/arch/cc.h
+++ tinyusb_net/tinyusb_net/arch/cc.h
@@ -72,10 +72,10 @@ typedef int sys_prot_t;
 
 #define LWIP_PLATFORM_ASSERT(x) do { if(!(x)) while(1); } while(0)
 
-unsigned int pico_lwip_rand(void);
 #ifndef LWIP_RAND
-// Use ROSC based random number generation, more for the fact that rand() may not be seeded, than anything else
-#define LWIP_RAND pico_lwip_rand
+#include "pico/rand.h"
+// Use the pico_rand library which goes to reasonable lengths to try to provide good entropy
+#define LWIP_RAND() get_rand_32()
 #endif
 
 #endif /* __CC_H__ */
